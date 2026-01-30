c864ec4ba1436c64a50a93891828834b77f18b2f
Adam Rozman <adam.rozman@est.tech>
2026-01-30 08:57:06 +0200

    introducing initial version of plugins
    Signed-off-by: Adam Rozman <adam.rozman@est.tech>

---
+++ ironic_wakeup_plugin/ironic_wakeup_plugin/redfish_wakeup.py
@@ -0,0 +1,31 @@
+# Copyright 2025 Ericsson Software Technology
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+import ironic.drivers.redfish as redfish
+import ironic.drivers.modules.fake as fake
+from ironic_wakeup_plugin.boot import SSHWakeup
+
+
+class RedfishWakeupHardware(redfish.RedfishHardware):
+    """Extended Redfish hardware type that can wake up dormant IPA"""
+
+    @property
+    def supported_boot_interfaces(self):
+        """List of supported boot interfaces."""
+        return (super().supported_boot_interfaces + [SSHWakeup])
+
+    @property
+    def supported_power_interfaces(self):
+        """List of supported power interfaces."""
+        return (super().supported_boot_interfaces + [fake.FakePower])
